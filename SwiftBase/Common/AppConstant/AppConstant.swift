a806467bd1418cc2ea211d202907157bb35a8962
TuanNV <tuannv@vtn-global.com>
2026-06-18 13:56:11 +0700

    set up flavor

--- SwiftBase/Common/AppConstant/AppConstant.swift
+++ SwiftBase/Common/AppConstant/AppConstant.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import Foundation
+
+struct AppConstants {
+    static let appId = ""
+    static let policyLink = ""
+    static let appLi = "https://apps.apple.com/app/id\(appId)"
+}
